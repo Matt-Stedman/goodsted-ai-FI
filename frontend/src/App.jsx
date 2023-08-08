0d2835627fe510e1dd6597141c2309f4dbd01334
Matthew <matthew@sparkmate.co>
2023-08-08 20:57:16 +0200

    Scoring and opportunities done !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,26 @@
+// Load react elements
 import React, { useEffect, useState } from "react";
+import { Box, Tab, Tabs } from "@mui/material";
+import { TabContext, TabPanel } from "@mui/lab";
+import ScoreWindow from "./components/ScoreWindow";
+
+// Load AI functions
+import { scoreOpportunityAgainstProfile } from "./functions/OpenAi";
+
+// Load styles
 import "./styles/App.css";
 
+// Load data (will be on a backend soon enough)
 import opportunities from "./data_tmp/opportunities.json";
 import profiles from "./data_tmp/profiles.json";
-import { TabContext, TabPanel } from "@mui/lab";
-import { Box, Tab, Tabs } from "@mui/material";
+import Opportunity from "./components/Opportunity";
 
 const App = () => {
     const [batchOpportunities, setBatchOpportunities] = useState([]);
-    const [batchScores, setBatchScores] = useState([]);
+    const [batchScores, setBatchScores] = useState({});
     const [currentOpportunity, setCurrentOpportunity] = useState("0");
-    const [hoveredOpportunity, setHoveredOpportunity] = useState("0");
+
+    const [profile, setProfile] = useState(profiles[0]);
 
     /**
      * Handle the changing of tabs
@@ -21,30 +31,44 @@ const App = () => {
         }, 100); // Delayed update to wait for 500ms after the action is completed
     };
 
+    // Helper function
     const displayOpportunities = () => {
-        return batchOpportunities.length > 0;
+        return Object.keys(batchScores).length > 0;
     };
 
+    // Load up the opportunities
     useState(() => {
-        setTimeout(() => {
-            setBatchOpportunities(opportunities.slice(0, 10));
-        }, 1000); // Delayed update to wait for 500ms after the action is completed
+        setBatchOpportunities(opportunities.slice(0, 10));
     }, []);
+
+    // Load up the scores
+    useEffect(() => {
+        const updateScores = async () => {
+            const updatedBatchScores = { ...batchScores }; // Create a copy of batchScores
+
+            const scorePromises = batchOpportunities.map(async (each_op) => {
+                if (updatedBatchScores[each_op.id] == null) {
+                    const each_score = await scoreOpportunityAgainstProfile(each_op, profile);
+                    console.log(`For ${each_op.id} the score is ${each_score}`);
+                    updatedBatchScores[each_op.id] = each_score; // Update the score for the opportunity ID
+                }
+            });
+
+            await Promise.all(scorePromises); // Wait for all scores to be calculated
+            console.log("updatedBatchScores: ", updatedBatchScores);
+            setBatchScores(updatedBatchScores); // Set the updated batchScores
+        };
+
+        updateScores();
+    }, [batchOpportunities]);
 
     return (
         <div className="App">
-            <header className="lock-header"></header>
-            <header className="header">
-                <h1>
-                    <span className="hardstuff">Hard Stuff </span>
-                </h1>
-                <p> Accelerating Hardware Solutions for a Sustainable Future</p>
-            </header>
             {displayOpportunities() ? (
                 <TabContext value={`${currentOpportunity}`}>
                     <Box sx={{ borderBottom: 1, borderColor: "divider", display: "flex", justifyContent: "center" }}>
                         <Tabs value={`${currentOpportunity}`} onChange={handleTabChange}>
-                            {batchOpportunities.map((opportunity, index) => (
+                            {Object.keys(batchScores).map((id, index) => (
                                 <Tab
                                     label={`${index}` === currentOpportunity ? "🟢" : "🔴"}
                                     value={`${index}`}
@@ -53,28 +77,22 @@ const App = () => {
                             ))}
                         </Tabs>
                     </Box>
-                    {batchOpportunities.map((opportunity, index) => (
-                        <TabPanel style={{ padding: 10, alignContent: "center" }} value={`${index}`} key={index}>
-                            <div>Beans!</div>
+                    {Object.keys(batchScores).map((id, index) => (
+                        <TabPanel
+                            style={{ padding: 10, justifyContent: "center", margin: "auto", width: "50%" }}
+                            value={`${index}`}
+                            key={index}
+                        >
+                            <ScoreWindow scores={batchScores[id]} />
+                            <Opportunity opportunity={batchOpportunities.find((op) => op.id === id)} />
                         </TabPanel>
                     ))}
                 </TabContext>
             ) : (
-                <div>Loading</div>
-            )}
-
-            <footer className="footer">
-                <div className="content">
-                    <div className="footer-links">
-                        {/* <a href="#careers">Careers</a> */}
-                        {/* <a href="#contact">Contact</a> */}
-                    </div>
-                    <p>
-                        &copy; {new Date().getFullYear()} <span className="hardstuff">Hard Stuff</span>. All rights
-                        reserved.
-                    </p>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "95vh" }}>
+                    Loading
                 </div>
-            </footer>
+            )}
         </div>
     );
 };
--- frontend/src/functions/OpenAi.js
+++ frontend/src/functions/OpenAi.js
@@ -10,46 +10,80 @@ const chatGPTClient = axios.create({
     },
 });
 
-const sterilizeFormData = (raw_form_data) => {
-    let form_data = {};
-    for (const key in raw_form_data) {
-        if (Object.prototype.hasOwnProperty.call(raw_form_data, key)) {
-            const value = raw_form_data[key];
-            if (typeof value === "string") {
-                form_data[key] = value.replace(/"/g, "'").replace(/\n/g, " ");
-            } else {
-                form_data[key] = value;
-            }
-        }
-    }
-    return form_data;
-};
+export const opportunity_elements_AI = ["problem", "need", "about", "plan", "why", "description"];
+export const profile_elements_AI = [
+    "about",
+    "openTo",
+    "skills",
+    "industryInterests",
+    // "pastOpportunityScores", // I want to unpack previous high scores (>7) and the rational behind those scores
+];
 
 /**
  * Function to create general feedback for the entire posting
  */
-export async function reviewEntireOpportunityGenerally(opportunity, profile) {
+export async function scoreOpportunityAgainstProfile(opportunity, profile) {
+    // both opportunity and profile come in as a plain dict
+
+    console.log("Opportunity: ", opportunity);
+    console.log("Profile: ", profile);
+
+    // STEP 1 - Unpack the relevant opportunity and profile elements as a string
+    const opportunity_string = opportunity_elements_AI.map((op) => `${op}: ${opportunity[op]}\n`);
+    const profile_string = profile_elements_AI.map((pr) => `${pr}: ${profile[pr]}\n`);
+
+    // STEP 2 - Compile the prompt
     const prompt = `
-Help me decide if this person's profile would be a good or bad fit for this volunteering opportunity. You must return your response as a json format:
-{
-    score: [0-10]
-    reason: [NO MORE THAN 50 WORDS]
-}
-
-OPPORTUNITY:
-${opportunity}
-
-PROFILE:
-${profile}
+    I need a rigorous evaluation of my volunteering profile's fit with this opportunity. Please be critical and precise, aiming to identify only the best-aligned opportunities.
+    Your feedback should be data-driven and rigorous, with a focus on strong alignment, and personal to me and my profile, using the second person!
+    
+    Evaluate my profile's elements based on a JSON-parsable format:
+    {
+        ${profile_elements_AI.map(
+            (pr) => `"${pr}": {
+            "score": [0 to 100],
+            "reason": [Up to 20 words]
+        },
+        `
+        )}
+    }
+    
+    MY PROFILE:
+    ${profile_string}
+    
+    OPPORTUNITY:
+    ${opportunity_string}
+    
         `;
 
+    console.log(prompt);
+    // STEP 3 - Get the results
     const response = await chatGPTClient.post("/", {
         messages: [{ role: "user", content: prompt }],
         model: "gpt-3.5-turbo",
-        max_tokens: 100,
     });
+
+    // TODO STEP 4 - Unpack the pastOpportunityScores
+
+    // TODO STEP 5 - Put in the 👍 and 👎 scores, either here or as weights on the other side?
 
     console.log(response);
 
-    return response.data.choices[0].message.content;
+    try {
+        var scores = JSON.parse(response.data.choices[0].message.content);
+
+        // Calculate the average score
+        var totalScores = Object.values(scores).reduce((total, sc) => total + sc.score, 0);
+        var numberOfCategories = Object.keys(scores).length;
+        var averageScore = totalScores / numberOfCategories;
+
+        scores["average"] = { score: averageScore };
+
+        console.log("Average Score:", averageScore);
+
+        return scores;
+    } catch {
+        console.log(`Failed to get a scoring for ${opportunity.id}`);
+        return null;
+    }
 }
